27a14912064653956880e8a598627b555ccfd798
wzq <297001985@qq.com>
2015-07-17 16:43:42 +0800

    update

--- app/src/main/java/com/wzq/material/activity/DetailActivity.java
+++ app/src/main/java/com/wzq/material/activity/DetailActivity.java
@@ -1,12 +1,15 @@
 package com.wzq.material.activity;
 
+import android.annotation.TargetApi;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.graphics.Palette;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import com.wzq.material.R;
@@ -23,8 +26,10 @@ public class DetailActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setBodyView(R.layout.activity_detail);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         imageView = (ImageView) findViewById(R.id.imageView);
-        ViewCompat.setTransitionName(imageView, "image");
+
+        //ViewCompat.setTransitionName(imageView, "image");
         ViewCompat.setTransitionName(toolbar, "title");
         setTitle(getIntent().getStringExtra("title"));
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), MainActivity.pictures[getIntent().getIntExtra("index", 0)]);
@@ -42,12 +47,17 @@ public class DetailActivity extends BaseActivity {
                 }
             }
         });
+        imageView.setVisibility(View.VISIBLE);
+        imageView.setScaleX(0);
+        imageView.setScaleY(0);
+        imageView.animate().scaleX(1).scaleY(1).setInterpolator(AnimationUtils.loadInterpolator(this, android.R.interpolator.fast_out_slow_in)).setStartDelay(300).start();
         baseLoading.setVisibility(View.GONE);
     }
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        finish();
+        finishAfterTransition();
         return super.onOptionsItemSelected(item);
     }
 }
